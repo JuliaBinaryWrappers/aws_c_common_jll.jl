f5ef1e06fc6c04e4f66090b6c0c153557b6a9771
jlbuild <juliabuildbot@gmail.com>
2023-09-21 00:45:26 +0000

    aws_c_common_jll build 0.9.3+1

--- src/wrappers/armv7l-linux-musleabihf.jl
+++ src/wrappers/armv7l-linux-musleabihf.jl
@@ -2,12 +2,13 @@
 export libaws_c_common
 
 JLLWrappers.@generate_wrapper_header("aws_c_common")
-JLLWrappers.@declare_file_product(libaws_c_common)
+JLLWrappers.@declare_library_product(libaws_c_common, "libaws-c-common.so.1")
 function __init__()
     JLLWrappers.@generate_init_header()
-    JLLWrappers.@init_file_product(
+    JLLWrappers.@init_library_product(
         libaws_c_common,
-        "lib/libaws-c-common.a",
+        "lib/libaws-c-common.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@generate_init_footer()
